acb4bc4def5d5e6dc61d04a494211f3bd1aad077
JiwoongSong <songjiwoong1020@gmail.com>
2025-10-20 19:56:55 +0900

    .

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,12 @@ import 'swiper/css/pagination';
 import './styles.css';
 
 const navBarList = ['시작', '캘린더', '소개'];
+const mainImgList = ['main_img_001', 'main_img_002', 'main_img_003', 'main_img_004'];
 
 export default function App() {
   return (
     <>
+      
       <Swiper
         spaceBetween={50}
         onSlideChange={() => console.log('slide change')}
@@ -18,13 +20,16 @@ export default function App() {
         pagination={{
           clickable: true,
           renderBullet: function (index, className) {
-            console.log(className)
-            return `<span class="${className}" style="display: inline-block">${navBarList[index]}</span>`;
+            return index === 0
+              ? `<span class='${className} fontColorWhite'>${navBarList[index]}</span>`
+              : `<span class='${className} fontColorBlack'>${navBarList[index]}</span>`;
           },
+          verticalClass: 'navBar'
         }}
         mousewheel={{ forceToAxis: true }}
         modules={[Pagination, Mousewheel]}
         className="mySwiper"
+        speed={1000}
       >
         <SwiperSlide>
           <div className='mainSection'>
@@ -34,18 +39,11 @@ export default function App() {
               rewind={true}
               modules={[Autoplay]}
             >
-              <SwiperSlide>
-                <div className='main_img main_img_001'></div>
-              </SwiperSlide>
-              <SwiperSlide>
-                <div className='main_img main_img_002'></div>
-              </SwiperSlide>
-              <SwiperSlide>
-                <div className='main_img main_img_003'></div>
-              </SwiperSlide>
-              <SwiperSlide>
-                <div className='main_img main_img_004'></div>
-              </SwiperSlide>
+              {mainImgList.map(v => (
+                <SwiperSlide>
+                  <div className={'main_img ' + v}></div>
+                </SwiperSlide>
+              ))}
             </Swiper>
             <div className='mainSection_content'>
               <p>동성 축구팀 파이팅!!</p>
@@ -54,6 +52,7 @@ export default function App() {
         </SwiperSlide>
         <SwiperSlide>캘린더 영역</SwiperSlide>
         <SwiperSlide>소개 영역</SwiperSlide>
+        <div className='blurBox'></div>
       </Swiper>
     </>
   );
