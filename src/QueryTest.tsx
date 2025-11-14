2598891de7e509ba6ea1d985446f6d6d1b58f91b
JiwoongSong <songjiwoong1020@gmail.com>
2025-11-14 18:00:51 +0900

    .

--- src/QueryTest.tsx
+++ src/QueryTest.tsx
@@ -5,7 +5,7 @@ const QueryTest = () => {
     queryKey: ['queryTest'],
     queryFn: async () => {
       const response = await fetch(
-        'http://localhost:3001',
+        'http://localhost:3000/getEventList',{method: 'get'}
       )
       return await response.json()
     },
@@ -13,6 +13,7 @@ const QueryTest = () => {
   return (
     <>
     {JSON.stringify(data)}
+    {/* {data} */}
     </>
   )
 }
